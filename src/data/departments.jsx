1d3e8567cde8c639fb080128f07a46939493be91
Dequé <carolinedeque@gmail.com>
2022-08-03 10:30:21 +0200

    Ajout Data STate/Department

---
+++ src/data/departments.jsx
@@ -0,0 +1,21 @@
+/**
+ * @return department Data for the department dropdown
+ */
+
+export const departments = [
+  {
+    name: "Sales",
+  },
+  {
+    name: "Marketing",
+  },
+  {
+    name: "Engineering",
+  },
+  {
+    name: "Human Resources",
+  },
+  {
+    name: "Legal",
+  },
+];
