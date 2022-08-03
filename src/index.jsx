e6e3732837405b889e64172042f9f1845bd9b6ca
Dequé <carolinedeque@gmail.com>
2022-08-03 10:28:25 +0200

    First Commit

---
+++ src/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import EmployeeList from "./pages/EmployeeList";
+import Home from "./pages/Home";
+import GlobalStyle from "./utils/style/GlobalStyle";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <GlobalStyle />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/employeeList" element={<EmployeeList />} />
+      </Routes>
+      {/* <Footer /> */}
+    </Router>
+  </React.StrictMode>
+);
